e3b1035b2cf5a4b8309b581d87d0589614765dcd
Ariel Fernandez <78383381+alphdevcode@users.noreply.github.com>
2024-05-19 00:46:24 -0400

    chore: set up initial top down features

---
+++ Source/DiabloLikeARPG/DiabloLikeARPG.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DiabloLikeARPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DiabloLikeARPG, "DiabloLikeARPG" );
+
+DEFINE_LOG_CATEGORY(LogDiabloLikeARPG)
+ 
