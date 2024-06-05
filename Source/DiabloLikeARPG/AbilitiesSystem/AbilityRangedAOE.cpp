c5cd421a6d4d1421f1965616b3fb432c577ba4c5
Ariel Fernandez <78383381+alphdevcode@users.noreply.github.com>
2024-06-05 00:07:35 -0400

    feat: implement grux enemy boss with melee attack and ranged ability

---
+++ Source/DiabloLikeARPG/AbilitiesSystem/AbilityRangedAOE.cpp
@@ -0,0 +1,33 @@
+// Copyright 2024 AlphDevCode. All Rights Reserved.
+
+
+#include "AbilityRangedAOE.h"
+
+#include "AbilityAOE.h"
+#include "DiabloLikeARPG/DiabloLikeARPGAnimInstance.h"
+#include "GameFramework/Character.h"
+
+void AAbilityRangedAOE::Initialize()
+{
+	Super::Initialize();
+}
+
+void AAbilityRangedAOE::AbilityActivated()
+{
+	Super::AbilityActivated();
+
+	CasterAnimInstance->OnAnimNotifyAttackPeak
+		.AddDynamic(this, &AAbilityRangedAOE::HandleAbilityEffectsSpawning);
+}
+
+void AAbilityRangedAOE::HandleAbilityEffectsSpawning()
+{
+	if(AController* CasterController = Caster->GetController(); CasterController != nullptr)
+	{
+		CasterController->StopMovement();
+	}
+
+	SpawnAbilityEffectsWithLocation(Target->GetActorLocation() + FVector(0, 0, 70));
+	CasterAnimInstance->OnAnimNotifyAttackPeak
+		.RemoveDynamic(this, &AAbilityRangedAOE::HandleAbilityEffectsSpawning);
+}
---
+++ Source/DiabloLikeARPG/AbilitiesSystem/AbilityRangedAOE.h
@@ -0,0 +1,23 @@
+// Copyright 2024 AlphDevCode. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilityWithSingleAnimationBase.h"
+#include "AbilityRangedAOE.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIABLOLIKEARPG_API AAbilityRangedAOE : public AAbilityWithSingleAnimationBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void Initialize() override;
+	virtual void AbilityActivated() override;
+
+	UFUNCTION()
+	void HandleAbilityEffectsSpawning();
+};
