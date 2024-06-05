22ebb52acf5745782885869efa31fe3e41cf6c3b
Ariel Fernandez <78383381+alphdevcode@users.noreply.github.com>
2024-06-05 00:33:05 -0400

    feat: create ability effect sound and implement it for grux roar explosion

---
+++ Source/DiabloLikeARPG/AbilityEffectSound.h
@@ -0,0 +1,31 @@
+// Copyright 2024 AlphDevCode. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitiesSystem/AbilityEffect.h"
+#include "AbilityEffectSound.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIABLOLIKEARPG_API AAbilityEffectSound : public AAbilityEffect
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	USoundBase* AbilitySound;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+	bool bShouldAttachToCaster = false;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, meta = (EditCondition = "bShouldAttachToCaster == true", EditConditionHides))
+	FName AttachPointName;
+
+private:
+	void DestroyEffect();
+};
